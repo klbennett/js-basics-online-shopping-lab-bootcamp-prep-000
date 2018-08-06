082f510e6c2b498e8ce864dd404e7502ca2cb523
Kay B <kaylouisebennett@gmail.com>
2018-08-06 10:14:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+let cart = [];
 
 function getCart() {
  return cart;
@@ -9,16 +9,41 @@ function setCart(c) {
   return cart;
 }
 
+let newObj = {};
 function addToCart(item) {
- // write your code here
+  newObj.name = item;
+  newObj.price = Math.floor(Math.random()*101);
+  cart.push(newObj);
+  return `${item} has been added to your cart.`;
 }
 
+var cartBegin = "In your cart, you have ";
+var i = "";
+
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+  }
+  else if (cart.length > 0) {
+    while (i = 0, i < (cart.length - 1), i++) {
+      var contents = `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`;
+      cartBegin += contents;
+    }
+    cartBegin.push(` and ${cart.item[cart.length-1]} at ${cart.price[cart.length-1]}.`)
+    return cartBegin;
+}
+
+{ 
+  
+  
+}
 }
 
 function total() {
-  // write your code here
+  if (cart.hasOwnProperty(price)) {
+    thePrice += cart[price];
+    return thePrice;
+}
 }
 
 function removeFromCart(item) {
